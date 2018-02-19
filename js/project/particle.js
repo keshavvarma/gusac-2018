ca1a4783b8215dceb5c6f36e4b592bf339ad8b5e
KESHAV VARMA <keshavvarma.vegiraju@gmail.com>
2018-02-19 06:47:03 +0530

    updated landing page with particle onhover animations and done with laptop view navbar

--- js/project/particle.js
+++ js/project/particle.js
@@ -1,29 +1,37 @@
-var numberOfEls = 500;
-    var duration = 2000;
-    var midScreenX = window.innerWidth / 2;
-    var midScreenY = window.innerHeight / 2;
-    var radius = Math.sqrt(midScreenX * midScreenX + midScreenY * midScreenY);
-    var fragment = document.createDocumentFragment();
+$(document).ready(function(){
+  
+  var mousePos = {};
 
-    for (var i = 0; i < numberOfEls; i++) {
-      var hue = Math.round(360 / numberOfEls * i);
-      var angle = Math.random() * Math.PI * 2;
-      var el = document.createElement('canvas');
-      el.style.backgroundColor = 'hsl(' + hue + ', 40%, 60%)';
-      el.style.width = '1px';
-      el.style.height = '1px';
-      anime({
-        targets: el,
-        width: ['1px', '10px'],
-        height: ['1px', '10px'],
-        left: [midScreenX + 'px', Math.cos(angle) * radius + midScreenX + 'px'],
-        top: [midScreenY + 'px', Math.sin(angle) * radius + midScreenY + 'px'],
-        delay: (duration / numberOfEls) * i,
-        duration: duration,
-        easing: 'easeInExpo',
-        loop: true
-      });
-      fragment.appendChild(el);
+ function getRandomInt(min, max) {
+   return Math.round(Math.random() * (max - min + 1)) + min;
+ }
+  
+  $(window).mousemove(function(e) {
+    mousePos.x = e.pageX;
+    mousePos.y = e.pageY;
+  });
+  
+  $(window).mouseleave(function(e) {
+    mousePos.x = -1;
+    mousePos.y = -1;
+  });
+  
+  var draw = setInterval(function(){
+    if(mousePos.x > 0 && mousePos.y > 0){
+      
+      var range = 115;
+      
+      var color = "background: rgb("+getRandomInt(0,255)+","+getRandomInt(0,255)+","+getRandomInt(0,255)+");";
+      
+      var sizeInt = getRandomInt(10, 30);
+      size = "height: " + sizeInt + "px; width: " + sizeInt + "px;";
+      
+      var left = "left: " + getRandomInt(mousePos.x-range-sizeInt, mousePos.x+range) + "px;";
+      
+      var top = "top: " + getRandomInt(mousePos.y-range-sizeInt, mousePos.y+range) + "px;"; 
+
+      var style = left+top+color+size;
+      $("<div class='ball' style='" + style + "'></div>").appendTo('body').one("webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend", function(){$(this).remove();}); 
     }
-
-    document.body.appendChild(fragment);
+  }, 1);
+});
